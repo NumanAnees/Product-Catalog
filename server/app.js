32e7dbd5ca1441e06fbdb88d35f8b0d986524d47
NumanAnees <numananees44@gmail.com>
2022-06-15 21:46:37 +0500

    Register created

--- server/app.js
+++ server/app.js
@@ -4,19 +4,25 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv").config();
 const path = require("path");
-
+var { User } = require("./models");
 const app = express();
 
 //db connection
-
-app.get("/", (req, res) => {
-  return res.json("hye");
-});
 
 //app-middlewares
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cors({ origin: process.env.CLIENT_URL }));
+
+app.post("/register", async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+  const user = await User.create({ firstName, lastName, email, password });
+  return res.json(user.id);
+});
+app.get("/", async (req, res) => {
+  const All = await User.findAll();
+  return res.json(All);
+});
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`API is running on port ${port}`));
