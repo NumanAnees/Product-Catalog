2d9545004fa6379877edcb05230a667e173e39bb
NumanAnees <numananees44@gmail.com>
2022-06-16 15:08:02 +0500

    Login Refactored

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,24 +1,58 @@
 const e = require("cors");
 const { User } = require("../models");
+const jwt = require("jsonwebtoken");
+
+//--------------------------------Register middleware--------------------------
 
 exports.register = async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
-  const user = await User.create({ firstName, lastName, email, password });
-  return res.json(user);
+  // const user = await User.create({ firstName, lastName, email, password });
+  // return res.json(user);
+
+  const alreadyExistsUser = await User.findOne({ where: { email } }).catch(
+    (err) => {
+      console.log("Error: ", err);
+    }
+  );
+
+  if (alreadyExistsUser) {
+    return res.status(409).json({ message: "User with email already exists!" });
+  }
+
+  const newUser = new User({ firstName, lastName, email, password });
+  const savedUser = await newUser.save().catch((err) => {
+    console.log("Error: ", err);
+    res.status(500).json({ error: "Cannot register user at the moment!" });
+  });
+
+  if (savedUser) res.json({ message: "Thanks for registering" });
 };
 
-exports.login = (req, res) => {
+//-------------------------------Login Middleware--------------------------------
+
+exports.login = async (req, res) => {
   const { email, password } = req.body;
-  const user = User.findOne({ where: { email } });
-  if (!user) {
-    return res.status(400).json({
-      error: "Email and password do not match",
-    });
-  }
-  if (!user.password == password) {
-    return res.status(400).json({
-      error: "Email and password matched",
-    });
-  }
-  return res.status(400).json(user);
+
+  const userWithEmail = await User.findOne({ where: { email } }).catch(
+    (err) => {
+      console.log("Error: ", err);
+    }
+  );
+
+  if (!userWithEmail)
+    return res
+      .status(400)
+      .json({ message: "Email or password does not match!" });
+
+  if (userWithEmail.password !== password)
+    return res
+      .status(400)
+      .json({ message: "Email or password does not match!" });
+
+  const jwtToken = jwt.sign(
+    { id: userWithEmail.id, email: userWithEmail.email },
+    "helloworld123hello"
+  );
+
+  res.json({ message: "Welcome Back!", token: jwtToken });
 };
